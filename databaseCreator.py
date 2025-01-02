9cbc63e8b0e913fd444464cfaeb68670efc8c406
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-02 12:17:03 +0100

    Added databaseCreator

---
+++ databaseCreator.py
@@ -0,0 +1,74 @@
+from langchain_community.document_loaders import JSONLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+
+def createVectorStore(
+    *,
+    destitationFolder: str = "./chroma_cve_db",
+    createAgain: bool = False,
+    embedder: str = "HuggingFaceEmbeddings",
+) -> None:
+    """
+    Create a vector store with the CVEs.
+
+    Args:
+        - destitationFolder (str): The folder where the vector store will be saved.
+        - createAgain (bool): If True, the vector store will be created again,
+            even if it already exists.
+        - embedder (str): The embedder to use. It can be "HuggingFaceEmbeddings" or "OpenAIEmbeddings".
+
+    Returns:
+        - None
+    """
+    if os.path.exists(destitationFolder) and not createAgain:
+        print(f"\t- indice {destitationFolder} ya existe")
+
+    else:
+        folder = os.path.dirname(os.path.abspath(__file__))
+
+        print("Cargando documentos ...")
+        loader = JSONLoader(
+            file_path=os.path.join(folder, "data", "techniquesEnterpriseAttack.json"),
+            jq_schema=".[]",
+            text_content=False,
+        )
+
+        docs = loader.load()
+        print("\t- {} documentos cargados".format(len(docs)))
+
+        print("Procesando documentos ...")
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=50)
+        all_splits = text_splitter.split_documents(docs)
+        print("\t- {} chunks creados".format(len(all_splits)))
+
+        print("Indexando documentos ...")
+
+        if embedder == "OpenAIEmbeddings":
+            load_dotenv()  # carga OPENAI_API_KEY del fichero .env
+            embeddings = OpenAIEmbeddings(
+                model="text-embedding-3-small", api_key=os.environ["OPENAI_API_KEY"]
+            )
+        elif embedder == "HuggingFaceEmbeddings":
+            embeddings = HuggingFaceEmbeddings(
+                model_name="sentence-transformers/all-MiniLM-L6-v2"
+            )
+        else:
+            raise ValueError(f"El embedder {embedder} no es válido")
+
+        vector_store = Chroma(
+            collection_name="cve_collection",
+            embedding_function=embeddings,
+            persist_directory=destitationFolder,  # Where to save data locally, remove if not necessary
+        )
+
+        vector_store.add_documents(documents=all_splits)
+        print(f"\t- indice {destitationFolder} creado")
+
+
+if __name__ == "__main__":
+    createVectorStore()
