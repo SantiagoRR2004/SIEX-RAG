156c2ea902f44d1a5a417647b6f11fa6451fd772
Sergio <sergiogonzalezrodriguez05@gmail.com>
2025-01-22 19:54:32 +0100

    chat con memoria bueno

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import databaseCreator
+from typing import Sequence
+from langchain_core.messages import BaseMessage, AIMessage, HumanMessage, SystemMessage
+from langgraph.graph.message import add_messages
 from langgraph.graph import START, StateGraph
-from typing_extensions import List, TypedDict
+from typing_extensions import List, TypedDict, Annotated
 from langchain_core.documents.base import Document
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
 from dotenv import load_dotenv
 import sys
 import os
@@ -30,16 +34,19 @@ def createChatbot() -> StateGraph:
                 + "If you don't know the answer, just say that you don't know. "
                 + "Use three sentences maximum and keep the answer concise.",
             ),
-            ("user", "Question: {question} \nContext: {context} \nAnswer:"),
+            # We will utilize MessagesPlaceholder to pass all the messages in.
+            MessagesPlaceholder(variable_name="messages"),
+            ("user", "Question: {question} \nContext: {context}"),
         ]
     )
 
     class State(TypedDict):
-        question: str
-        context: List[Document]
-        answer: str
+        messages: Annotated[
+            Sequence[BaseMessage], add_messages
+        ]  # The messages in the conversation.
+        context: List[Document]  # The context to answer the question.
 
-    def retrieveContext(state: State, debug: bool = False) -> List[Document]:
+    def retrieveContext(state: State, debug: bool = False) -> State:
         """
         Retrieve the context from the vector store.
 
@@ -48,19 +55,24 @@ def createChatbot() -> StateGraph:
             - debug (bool): If True, debug information will be printed.
 
         Returns:
-            - List[Document]: The context.
+            - State: The state with the retrieved context.
         """
-        retrieved_documents = vector_store.similarity_search(state["question"], k=5)
+        query = state["messages"][-1].content
+        retrieved_documents = vector_store.similarity_search(query, k=3)
         if debug:
             print(
                 "DEBUG::retrieveContext:: Num. documentos recuperados: {}".format(
                     len(retrieved_documents)
                 )
             )
+            for doc in retrieved_documents:
+                print(f"DEBUG:: Doc: {doc.page_content[:100]}...")
             print()
+
+        # We will pass the retrieved documents as context to the next step.
         return {"context": retrieved_documents}
 
-    def generateAnswer(state: State, debug: bool = False) -> str:
+    def generateAnswer(state: State, debug: bool = False) -> State:
         """
         Generate the answer to the question.
 
@@ -69,27 +81,33 @@ def createChatbot() -> StateGraph:
             - debug (bool): If True, debug information will be printed.
 
         Returns:
-            - str: The answer.
+            - State: The state with the answer.
         """
         docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+        query = state["messages"][-1].content
         messages = rag_prompt.invoke(
-            {"question": state["question"], "context": docs_content}
+            {
+                "messages": state["messages"][:-1],
+                "question": query,
+                "context": docs_content,
+            }
         )
 
         if debug:
             messages_text = str(messages)
             messages_start = messages_text[:150]
             messages_end = messages_text[len(messages_text) - 150 :]
-            print(
-                "DEBUG::generateAnswer:: Prompt: {} ... {}".format(
-                    messages_start, messages_end
-                )
-            )
+            # print(
+            #     "DEBUG::generateAnswer:: Prompt: {} ... {}".format(
+            #         messages_start, messages_end
+            #     )
+            # )
+            print("DEBUG::generateAnswer::" + messages_text)
             print("DEBUG::generateAnswer::")
             print()
 
         response = llm_rag.invoke(messages)
-        return {"answer": response.content}
+        return {"messages": response}
 
     graph_builder = StateGraph(State).add_sequence(
         [
@@ -98,12 +116,14 @@ def createChatbot() -> StateGraph:
         ]
     )
     graph_builder.add_edge(START, "retrieve")
-    graph = graph_builder.compile()
+    memory = MemorySaver()
+    graph = graph_builder.compile(checkpointer=memory)
     return graph
 
 
 def main():
     graph = createChatbot()
+    config = {"configurable": {"thread_id": "1111"}}
     print(
         "CHATBOT INICIADO.\nFinalizar sesión con los comandos :salir, :exit o :terminar"
     )
@@ -112,8 +132,9 @@ def main():
         if query.lower() in [":salir", ":exit", ":terminar"]:
             sys.exit("Gracias por hablar conmigo!!!!")
 
-        rag_response = graph.invoke({"question": query})
-        print("[Chatbot con RAG] " + rag_response["answer"])
+        query_message = [HumanMessage(query)]
+        rag_response = graph.invoke({"messages": query_message}, config)
+        rag_response["messages"][-1].pretty_print()
         print()
 
 
