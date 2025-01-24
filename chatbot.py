b25c8860f135e0f1f9aa3ab67bd7f5d717ebb207
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-24 13:56:45 +0100

    Completed serializeContext

--- chatbot.py
+++ chatbot.py
@@ -85,5 +85,45 @@ class MITREATTACKChatbot:
 
         return retrieved_docs
 
-    def serializeContext(self):
-        pass
+    def serializeContext(self) -> str:
+        """
+        Serialize the context.
+
+        It returns a string with
+        the metadata and the content of the documents.
+
+        The metadata had the folowing keys:
+            {id, url, seq_num, source}
+
+        Later we could eliminate the source attribute
+        because we only have on json.
+
+        The content is a string.
+
+        Args:
+            - None
+
+        Returns:
+            - str: The serialized context.
+        """
+        serializedDocuments = []
+
+        metadataKeys = []
+
+        for doc in self.context:
+
+            metadata = doc.metadata
+
+            serializedDocuments.append(
+                f"Source: {metadata}\nContent: {doc.page_content}"
+            )
+
+            if set(metadata.keys()) not in metadataKeys:
+                metadataKeys.append(set(metadata.keys()))
+
+        if self.verbose:
+            for keys in metadataKeys:
+                print(keys)
+            print()
+
+        return "\n\n".join(serializedDocuments)
