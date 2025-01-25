a91a4ffc54afe79450840c8e161dbf4a159c1232
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-25 16:14:18 +0100

    Added dataset

---
+++ test.py
@@ -0,0 +1,46 @@
+from datasets import load_dataset, concatenate_datasets
+import random
+
+
+def getInputAndOutput(example: dict) -> tuple:
+    """
+    Get the input and output of the example
+
+    Args:
+        - example (dict): The example from the dataset
+
+    Returns:
+        - tuple: The input and output of the example
+    """
+    text = example["text"]
+
+    # The input is the text between "<|user|>" and "<|end|>"
+    input = text.split("<|end|>")[0].split("<|user|>")[1].strip()
+
+    # The output is the text between "<|assistant|>" and "<|end|>"
+    output = text.split("<|end|>")[1].split("<|assistant|>")[1].strip()
+
+    # Remove the b''
+    input = input[2:-1]
+    output = output[2:-1]
+
+    return input, output
+
+
+if __name__ == "__main__":
+
+    # Load the dataset
+    dataset = load_dataset("dattaraj/security-attacks-MITRE")
+
+    # We get all the data together
+    data = concatenate_datasets([dataset["train"], dataset["validation"]])
+
+    # We get a random example
+    example = random.choice(data)
+
+    # We get the input and output of the example
+    input, output = getInputAndOutput(example)
+
+    print(f"{input}")
+
+    print(f"{output}")
